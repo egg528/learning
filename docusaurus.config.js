ad3b6943b894688aaee115cc30f289d38b5ac8a7
wkwon <rnjsdntjr26@daum.net>
2023-01-22 00:10:00 +0900

    2023-01-22(일): 커뮤니티 제거

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -98,18 +98,6 @@ const config = {
           {
             title: 'Community',
             items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
             ],
           },
           {
