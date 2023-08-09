52fad4e80863f48fe692a7f417f438dfd3c9cf81
wkwon <rnjsdntjr26@daum.net>
2023-08-09 21:44:25 +0900

    2023-08-09(수)

---
+++ src/components/resume/WorkExperience.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function WrokExperiences(): JSX.Element {
+    return (
+        <section id={'introduce'}>
+            <h2>Introduce<span className="period-mark">.</span></h2>
+            <p className="big-paragraph">
+                서울에서 7년차 웹 프론트엔드 엔지니어로 일하고 있으며 아름다운
+                인터페이스와 애니메이션을 구현하는 것을 좋아합니다. 개발자의 가치는
+                비즈니스 가치를 구현하는데서 나온다고 믿습니다. 주도적으로 업무를
+                진행할 수 있는 환경을 선호합니다.
+            </p>
+        </section>
+    );
+}
